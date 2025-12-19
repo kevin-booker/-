8c960469984642869075494db031847467e4ad92
kevin-booker <peterfong980821@gmail.com>
2025-12-19 14:04:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// 首頁按鈕跳到遊戲頁
+const startBtn = document.getElementById("startBtn");
+if (startBtn) {
+  startBtn.addEventListener("click", () => {
+    window.location.href = "game.html";
+  });
+}
 
+// 遊戲頁按鈕跳回首頁
+const backBtn = document.getElementById("backBtn");
+if (backBtn) {
+  backBtn.addEventListener("click", () => {
+    window.location.href = "index.html";
+  });
+}
